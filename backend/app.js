e4698ddc93e372991274b38b2f5833213ee6950c
Mahesh-010 <142183685+Mahesh-010@users.noreply.github.com>
2026-03-13 09:26:59 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app     = express();
+
+app.use(express.json());
+
+// Routes
+app.use('/auth',  require('./routes/authRoutes'));
+app.use('/books', require('./routes/bookRoutes'));
+app.use('/users', require('./routes/userRoutes'));
+
+module.exports = app;
