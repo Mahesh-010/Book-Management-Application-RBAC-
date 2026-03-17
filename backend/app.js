be94672ba4eea2598b840da89cf35a9323a9cada
Mahesh-010 <142183685+Mahesh-010@users.noreply.github.com>
2026-03-17 16:34:46 +0530

    Fix syntax error in users route import

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,6 @@ app.use(express.json());
 // Routes
 app.use('/auth',  require('./routes/authRoutes'));
 app.use('/books', require('./routes/bookRoutes'));
-app.use('/users' require('./routes/userRoutes'));
+app.use('/users', require('./routes/userRoutes'));
 
 module.exports = app;
